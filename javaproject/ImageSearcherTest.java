9d98717d3e46a69e299a9bdec76774126b830a9b
thugcreeper <nhpss100032@gmail.com>
2025-04-28 00:51:02 +0800

    搜尋圖片與放大功能
    目前是用upslash API去抓圖片，免費的但效果有點爛

---
+++ javaproject/ImageSearcherTest.java
@@ -0,0 +1,9 @@
+//測試imageSearcher、showZoomedImage的主程式
+import javax.swing.*;
+
+public class ImageSearcherTest {
+    public static void main(String[] args) {
+        ImageSearcher imageSearcher = new ImageSearcher();
+        imageSearcher.setVisible(true);
+    }
+}
