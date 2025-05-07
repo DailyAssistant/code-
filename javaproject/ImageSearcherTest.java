4ae7e7ee9d4c2f02a175119e2353833fa27226ef
thugcreeper <nhpss100032@gmail.com>
2025-05-08 00:09:50 +0800

    移除資料
    目前repostory有點亂，先移除一些資料夾

--- javaproject/ImageSearcherTest.java
+++
@@ -1,9 +0,0 @@
-//測試imageSearcher、showZoomedImage的主程式
-import javax.swing.*;
-
-public class ImageSearcherTest {
-    public static void main(String[] args) {
-        ImageSearcher imageSearcher = new ImageSearcher();
-        imageSearcher.setVisible(true);
-    }
-}
