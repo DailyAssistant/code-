36300b4bbff9a365330f0fa19dd8abd75771330f
fupeihsuan <fupeihsuan@gmail.com>
2025-05-18 18:41:08 +0800

    加使用者儲存圖片用的method

--- SaveImage/src/SaveImage.java
+++ SaveImage/src/SaveImage.java
@@ -3,11 +3,19 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-public final class SaveImage {
-    public static void save(BufferedImage img, String pathname, String format) {
+public class SaveImage {
+    public void save(BufferedImage img, String pathname, String format, boolean saveToHistory) {
         try {
+            if(saveToHistory){
+                pathname = "../../ImageHistory/images/" +
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + ".jpg";
+            }
+
             File imgFile = new File(pathname);
+
             if(imgFile.mkdirs()){
                 System.out.println("路徑建立成功");
             }
@@ -15,6 +23,33 @@ public final class SaveImage {
             System.out.println("儲存成功！");
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), "儲存失敗", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    //使用者儲存圖片用
+    public void saveImageWithDialog(BufferedImage img, String format) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("選擇儲存位置");
+        fileChooser.setSelectedFile(new File("image_" +
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")) + format)); // 預設檔名
+
+        int userSelection = fileChooser.showSaveDialog(null);
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+
+            // 確保副檔名存在
+            String path = fileToSave.getAbsolutePath();
+            if (!path.toLowerCase().endsWith("." + format)) {
+                fileToSave = new File(path + "." + format);
+            }
+
+            try {
+                ImageIO.write(img, format, fileToSave);
+                JOptionPane.showMessageDialog(null, "儲存成功：" + fileToSave.getAbsolutePath());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "儲存失敗：" + e.getMessage());
+            }
         }
     }
 }
--- SaveImage/src/TestSaveImage.java
+++ SaveImage/src/TestSaveImage.java
@@ -5,15 +5,33 @@ import java.net.URL;
 
 public class TestSaveImage {
     public static void main(String[] args) {
+        /*
         try {
             // 讀取網路圖片
             URL imageUrl = new URL("https://www.biaopan8.com/wp-content/uploads/2022/05/google_g_hero.jpg");
             BufferedImage image = ImageIO.read(imageUrl);
 
             // 儲存圖片至指定路徑，注意要包含檔名與副檔名
-            SaveImage.save(image, "../../ImageHistory/images/google_g_hero.jpg", "jpg");
+            SaveImage saveImage = new SaveImage();
+            saveImage.save(image, "../../ImageHistory/hello/google_g_hero.jpg", "jpg", false);
+
+            saveImage.save(image, "", "jpg", true);
         } catch (IOException e) {
             System.err.println("讀取圖片失敗：" + e.getMessage());
+        }
+        */
+
+        try {
+            // 從網路載入圖片
+            URL url = new URL("https://www.biaopan8.com/wp-content/uploads/2022/05/google_g_hero.jpg");
+            BufferedImage image = ImageIO.read(url);
+
+            // 儲存圖片，使用 GUI 選擇位置
+            SaveImage saveImage = new SaveImage();
+            saveImage.saveImageWithDialog(image, "jpg");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
